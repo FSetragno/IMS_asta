3f9bdead0f613a136e6b1bad9ee9ec21e5b83ff3
jacoban <jaco.banfi@gmail.com>
2015-05-11 16:33:55 +0200

    Second commit

---
+++ src/japaneseAuction/Auctioneer.java
@@ -0,0 +1,148 @@
+package japaneseAuction;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.core.AID;
+
+import java.util.*;
+
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class Auctioneer extends Agent{
+
+	List<AID> bidders = new ArrayList<AID>();
+	
+	public enum State {
+		START, ACTIVE
+	}
+	
+	State state = State.START;
+	
+	int price = 10;
+	int increment = 1;
+	
+	private MessageTemplate mt;
+	
+	protected void setup() {
+		System.out.println("Auctioneer-agent " + getAID().getName() + " started.");
+		Object[] args = getArguments();
+		if (args != null && args.length == 3) {
+			int biddersNumber = Integer.parseInt((String)args[0]);
+			for(int i = 0; i < biddersNumber; i++){
+				bidders.add(new AID("bidder" + Integer.toString(i), AID.ISLOCALNAME));
+			}
+			
+			String p = (String)args[1];
+			
+			String inc = (String)args[2];
+			
+			increment = Integer.parseInt(inc);
+			price = Integer.parseInt(p) - increment;
+			
+			System.out.println("Auctioneer-agent " + getAID().getName() + " starts from price = " + p + " units and will increment by " + inc + ".");
+			//Send immediately an inform message with the start of the auction
+			System.out.println("Press any key to start the auction.");
+			Scanner input = new Scanner(System.in);
+			String s = input.nextLine();
+			startAuction();
+			
+			addBehaviour(new PublishPriceBehaviour(this, 5000));
+
+			addBehaviour(new HandleCallsBehaviour());
+			
+		}
+		else {
+			System.out.println("No number of agents specified.");
+			doDelete();
+		}
+	}
+	
+	// Put agent clean-up operations here
+	protected void takeDown() {
+		System.out.println("Auctioneer-agent " + getAID().getName() + " terminating.");
+	}
+	
+	/**
+	This is invoked at the auctioneer setup
+	*/
+	private void startAuction() {
+		addBehaviour(new OneShotBehaviour() {
+			private static final long serialVersionUID = 1L;
+
+			public void action() {
+				ACLMessage start = new ACLMessage(ACLMessage.INFORM);
+				for (int i = 0; i < bidders.size(); ++i) {
+					start.addReceiver(bidders.get(i));
+				} 
+				start.setContent("START");
+				start.setConversationId("auction");
+				myAgent.send(start);
+			}
+		} );
+	}
+	
+	private class PublishPriceBehaviour extends TickerBehaviour {
+		
+		private static final long serialVersionUID = 1L;
+
+		public PublishPriceBehaviour(Agent a, long period) {
+			super(a, period);
+		}
+
+		@Override
+		protected void onTick() {
+			price += increment;
+			ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+			for (int i = 0; i < bidders.size(); ++i) {
+				cfp.addReceiver(bidders.get(i));
+			} 
+			cfp.setContent(Integer.toString(price));
+			cfp.setConversationId("bid");
+			cfp.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
+			System.out.println("Auctioneer-agent " + getAID().getName() + " is proposing " + Integer.toString(price) + ".");
+			myAgent.send(cfp);
+			// Prepare the template to get proposals
+			mt = MessageTemplate.and(MessageTemplate.MatchConversationId("bid"),
+				 MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+			
+		}
+		
+	}
+	
+	private class HandleCallsBehaviour extends CyclicBehaviour{
+
+		@Override
+		public void action() {
+			ACLMessage reply = myAgent.receive(mt);
+			if (reply != null){
+				String cont = reply.getContent();
+			
+				if (cont.equals("OUT")){
+					bidders.remove(reply.getSender());
+					System.out.println("Auctioneer-agent " + getAID().getName() + " received withdrawal of bidder-agent " + reply.getSender().getName() + ".");
+				}
+			
+				if (bidders.size() == 1){
+					System.out.println("End of the auction.");
+					System.out.println("The winner is " + bidders.get(0).getName() + ".");
+					System.out.println("The price to be paid for the item is " + Integer.toString(price) + ".");
+					
+					//Send a final INFORM message to the winner
+					ACLMessage win = new ACLMessage(ACLMessage.INFORM);
+					win.addReceiver(bidders.get(0)); 
+					win.setContent("WIN");
+					win.setConversationId("win");
+					myAgent.send(win);
+					
+					doDelete();
+				}
+			
+			}
+			else{
+				block();
+			}
+		}
+		
+	}
+}
---
+++ src/japaneseAuction/Bidder.java
@@ -0,0 +1,128 @@
+package japaneseAuction;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.core.AID;
+
+import java.util.*;
+
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class Bidder extends Agent{
+
+	AID auctioneer;
+	public enum State {
+	    WAITING, ACTIVE, EXITING 
+	}
+	
+	State state = State.WAITING;
+	int max_price;
+	int new_price;
+	
+	protected void setup() {
+		System.out.println("Bidder-agent " + getAID().getName() + " started.");
+		Object[] args = getArguments();
+		if (args != null && args.length == 2) {
+			auctioneer = new AID((String)args[0], AID.ISLOCALNAME);
+			String p = (String)args[1];
+			max_price = Integer.parseInt(p);
+			System.out.println("Bidder-agent " + getAID().getName() + " maximum bid: " + p);
+			addBehaviour(new BidderBehaviour());
+			
+						
+		}
+		else {
+			System.out.println("Error in the arguments.");
+			doDelete();
+		}
+	}
+	
+	// Put agent clean-up operations here
+	protected void takeDown() {
+		System.out.println("Bidder-agent " + getAID().getName() + " terminating.");
+	}
+	
+	private class BidderBehaviour extends CyclicBehaviour{
+		private static final long serialVersionUID = 1L;
+		MessageTemplate mt;
+		ACLMessage msg;
+		
+		private boolean receiveWin(){
+			mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+			msg = myAgent.receive(mt);
+			if (msg != null){
+				String content = msg.getContent();
+				if (content.equals("WIN")){
+					System.out.println("Bidder-agent " + getAID().getName() + " won the item for " + new_price + " units.");
+				}
+				else{
+					System.out.println("PANICO");
+				}
+				
+			}
+			else{
+				return true;
+			}
+			return false;
+		}
+		
+		public void action(){
+			switch(state){
+				case WAITING:
+					mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+					msg = myAgent.receive(mt);
+					if (msg != null) {
+						// CFP Message received. Process it
+						String content = msg.getContent();
+						if (content.equals("START")){
+							System.out.println("Bidder-agent " + getAID().getName() + " is now in the auction.");
+							state = State.ACTIVE;
+						}
+					}
+					else{
+						block();
+					}
+					break;
+				case ACTIVE:
+					mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+					msg = myAgent.receive(mt);
+					boolean blockcfp = false;
+					
+					if (msg != null) {
+						// CFP Message received. Process it
+						new_price = Integer.parseInt(msg.getContent());
+						ACLMessage reply = msg.createReply();
+
+						if (new_price > max_price) {
+							reply.setPerformative(ACLMessage.REFUSE);
+							reply.setContent("OUT");
+							state = State.EXITING;
+							//doDelete();
+						}
+						else {
+							reply.setPerformative(ACLMessage.INFORM);
+							reply.setContent("OK");
+						}
+						
+						myAgent.send(reply);
+					}
+					
+					else {
+						blockcfp = true;
+					}
+					
+					if (blockcfp && receiveWin()){
+						block();
+					}
+					break;
+					
+				case EXITING:
+					receiveWin(); //If two agents are even, the first wins!
+					System.out.println("Bidding-agent " + getAID().getName() + " is quitting the auction.");
+					doDelete();
+					break;
+			}
+		}
+	}
+}
