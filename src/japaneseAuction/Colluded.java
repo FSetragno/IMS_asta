eee00dd96f8549af4a8ba393ef06822837eadd92
jacoban <jaco.banfi@gmail.com>
2015-05-14 15:13:57 +0200

    Added colluded

---
+++ src/japaneseAuction/Colluded.java
@@ -0,0 +1,13 @@
+package japaneseAuction;
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.core.AID;
+
+import java.util.*;
+
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class Colluded extends Bidder{
+	
+}
